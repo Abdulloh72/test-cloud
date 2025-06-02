cb4588e7303c630d7763ab60618aed944d13dc99
Abdulloh22 <114293084+Abdulloh22@users.noreply.github.com>
2025-06-02 11:05:58 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello from backend!' });
+});
+
+app.listen(port, () => {
+  console.log('Server running on port 500');
+});
